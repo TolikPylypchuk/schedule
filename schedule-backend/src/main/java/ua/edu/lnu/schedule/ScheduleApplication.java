e2af5066c282f22c7d3f1f192b41917335ea1afe
Tolik Pylypchuk <pylypchuk.tolik@gmail.com>
2017-05-08 12:31:25 +0300

    Dropped OAuth 2.0 in favour of JWT

--- schedule-backend/src/main/java/ua/edu/lnu/schedule/ScheduleApplication.java
+++ schedule-backend/src/main/java/ua/edu/lnu/schedule/ScheduleApplication.java
@@ -4,32 +4,18 @@ import java.time.DayOfWeek;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 import ua.edu.lnu.schedule.infrastructure.*;
 
 @SpringBootApplication
-@EnableOAuth2Sso
-public class ScheduleApplication extends WebSecurityConfigurerAdapter {
+public class ScheduleApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ScheduleApplication.class, args);
 	}
 	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.antMatcher("/**")
-			.authorizeRequests()
-			.antMatchers("/", "/login**", "**.js", "**.css", "**.ico")
-			.permitAll()
-			.anyRequest()
-			.authenticated();
-	}
-
 	@Bean
 	public Jackson2ObjectMapperBuilder jacksonBuilder() {
 		return new Jackson2ObjectMapperBuilder()
