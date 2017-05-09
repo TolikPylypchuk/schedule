30453c29691eaf560b38b50381e02563cdfa8a3c
Tolik Pylypchuk <pylypchuk.tolik@gmail.com>
2017-05-09 12:15:50 +0300

    Modified the properties and configuration to include JWT and CORS

--- schedule-backend/src/main/java/ua/edu/lnu/schedule/ScheduleApplication.java
+++ schedule-backend/src/main/java/ua/edu/lnu/schedule/ScheduleApplication.java
@@ -1,13 +1,7 @@
 package ua.edu.lnu.schedule;
 
-import java.time.DayOfWeek;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
-
-import ua.edu.lnu.schedule.infrastructure.*;
 
 @SpringBootApplication
 public class ScheduleApplication {
@@ -16,10 +10,5 @@ public class ScheduleApplication {
 		SpringApplication.run(ScheduleApplication.class, args);
 	}
 	
-	@Bean
-	public Jackson2ObjectMapperBuilder jacksonBuilder() {
-		return new Jackson2ObjectMapperBuilder()
-			.serializerByType(DayOfWeek.class, new DayOfWeekSerializer())
-			.deserializerByType(DayOfWeek.class, new DayOfWeekDeserializer());
-	}
+	
 }
---
+++ schedule-backend/src/main/java/ua/edu/lnu/schedule/config/RestConfig.java
@@ -0,0 +1,39 @@
+package ua.edu.lnu.schedule.config;
+
+import java.time.DayOfWeek;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import ua.edu.lnu.schedule.infrastructure.DayOfWeekDeserializer;
+import ua.edu.lnu.schedule.infrastructure.DayOfWeekSerializer;
+
+@Configuration
+public class RestConfig {
+	@Bean
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("OPTIONS");
+		config.addAllowedMethod("GET");
+		config.addAllowedMethod("POST");
+		config.addAllowedMethod("PUT");
+		config.addAllowedMethod("DELETE");
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
+	}
+	
+	@Bean
+	public Jackson2ObjectMapperBuilder jacksonBuilder() {
+		return new Jackson2ObjectMapperBuilder()
+			.serializerByType(DayOfWeek.class, new DayOfWeekSerializer())
+			.deserializerByType(DayOfWeek.class, new DayOfWeekDeserializer());
+	}
+}
